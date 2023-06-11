fb64fc25051ff2b1df5c010cf1aca87c80b408fa
alisonpersil <alisonviana@live.com>
2023-06-11 15:05:55 -0300

    feat: add search term and prefix

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const readline = require('readline-sync')
 function start() {
-const content = {}
+    const content = {}
 
-content.searchTerm = askAndReturnSearchTerm()
+    content.searchTerm = askAndReturnSearchTerm()
+    content.prefix = askAndReturnPrefix()
+
 
 function askAndReturnSearchTerm() {
     return readline.question('Type a Wikipedia search term: ')
@@ -11,8 +13,10 @@ function askAndReturnSearchTerm() {
 function askAndReturnPrefix() {
     const prefixes = ['Who is', 'What is', 'The history of']
     const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option: ')
+    const selectedPrefixText = prefixes[selectedPrefixIndex]
 
-    console.log(selectedPrefixIndex)
+
+   return selectedPrefixText
 }
 console.log(content)
 
